b264644df20079e3e074f1b267ef0796c37f530d
jonchou <jonchou@ucdavis.edu>
2016-02-24 16:11:04 -0800

    Setup

---
+++ ProjectX/EventViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventViewController.swift
+//  ProjectX
+//
+//  Created by Jonathan Chou on 2/24/16.
+//  Copyright © 2016 Jonathan Chou. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
